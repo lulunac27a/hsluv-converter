9d82928a530a43e6683c3b12a56a25b00b0b0ef8
lulunac27a <nacerilucas@gmail.com>
2023-08-25 21:28:43 -0500

    Update hsluv-4096.py

--- hsluv-4096.py
+++ hsluv-4096.py
@@ -1,15 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import hsluv
-
-
-# In[2]:
-
-
 for i in range(16):
     for j in range(16):
         for k in range(16):
@@ -17,11 +6,6 @@ for i in range(16):
             print("H: "f"{hsluv.rgb_to_hsluv((i/15,j/15,k/15))[0]:>6.2f}")
             print("S: "f"{hsluv.rgb_to_hsluv((i/15,j/15,k/15))[1]:>6.2f}")
             print("L: "f"{hsluv.rgb_to_hsluv((i/15,j/15,k/15))[2]:>6.2f}")
-
-
-# In[3]:
-
-
 for i in range(16):
     for j in range(16):
         for k in range(16):
@@ -29,11 +13,6 @@ for i in range(16):
             print("H: "f"{hsluv.rgb_to_hpluv((i/15,j/15,k/15))[0]:>6.2f}")
             print("P: "f"{hsluv.rgb_to_hpluv((i/15,j/15,k/15))[1]:>6.2f}")
             print("L: "f"{hsluv.rgb_to_hpluv((i/15,j/15,k/15))[2]:>6.2f}")
-
-
-# In[4]:
-
-
 for i in range(1,10):
     for j in range(11):
         for k in range(36):
@@ -43,11 +22,6 @@ for i in range(1,10):
             print("R: "f"{hsluv.hsluv_to_rgb((k*10,j*10,i*10))[0]:>6.3f}")
             print("G: "f"{hsluv.hsluv_to_rgb((k*10,j*10,i*10))[1]:>6.3f}")
             print("B: "f"{hsluv.hsluv_to_rgb((k*10,j*10,i*10))[2]:>6.3f}")
-
-
-# In[5]:
-
-
 for i in range(1,10):
     for j in range(11):
         for k in range(36):
@@ -57,10 +31,3 @@ for i in range(1,10):
             print("R: "f"{hsluv.hpluv_to_rgb((k*10,j*10,i*10))[0]:>6.3f}")
             print("G: "f"{hsluv.hpluv_to_rgb((k*10,j*10,i*10))[1]:>6.3f}")
             print("B: "f"{hsluv.hpluv_to_rgb((k*10,j*10,i*10))[2]:>6.3f}")
-
-
-# In[ ]:
-
-
-
-
